9e72a5596a75f8450b75af9b40a975542c93086f
Thien Phuc <thienphuc484@gmail.com>
2024-04-21 14:03:08 +0700

    Add MovieDTO for data transfer.

---
+++ src/main/java/com/app/DTO/MovieDTO.java
@@ -0,0 +1,66 @@
+package com.app.DTO;
+
+import com.app.Entity.Director;
+import com.app.Entity.Genre;
+import com.app.Entity.Rating;
+
+import java.util.Date;
+import java.util.Set;
+
+public class MovieDTO {
+
+    private String title;
+    private String rating;
+    private Date release_year;
+    private Integer length;
+    private Director director;
+    private Set<Genre> genres;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Date getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(Date release_year) {
+        this.release_year = release_year;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public Set<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Set<Genre> genres) {
+        this.genres = genres;
+    }
+}
