9be08ee089110a8764bc85af908bdc21dd017512
Thien Phuc <thienphuc484@gmail.com>
2024-04-24 12:37:53 +0700

    Add Director Service.

---
+++ src/main/java/com/app/Service/DirectorService.java
@@ -0,0 +1,14 @@
+package com.app.Service;
+
+import com.app.DTO.DirectorDTO;
+import com.app.Entity.Director;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface DirectorService {
+    Flux<Director> findAll();
+    Mono<Director> findById(Long id);
+    Mono<Director> save(DirectorDTO directorDTO);
+    Mono<Director> update(Long id,DirectorDTO directorDTO);
+    Mono<Void> delete(Long id);
+}
