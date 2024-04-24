a40e194b7dd03a43b2d5a81348cb23ad2cb22420
Thien Phuc <thienphuc484@gmail.com>
2024-04-24 12:32:55 +0700

    Add Director DTO

---
+++ src/main/java/com/app/DTO/DirectorDTO.java
@@ -0,0 +1,45 @@
+package com.app.DTO;
+
+import com.app.Entity.Nationality;
+
+import java.time.LocalDate;
+
+public class DirectorDTO {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String nationality;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
