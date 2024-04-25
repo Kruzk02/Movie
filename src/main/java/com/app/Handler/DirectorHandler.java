2e90fde9360596720b72a0cc37a04807f66dc1b9
Thien Phuc <thienphuc484@gmail.com>
2024-04-25 11:46:30 +0700

    Add Director Handler and Router.

---
+++ src/main/java/com/app/Handler/DirectorHandler.java
@@ -0,0 +1,58 @@
+package com.app.Handler;
+
+import com.app.DTO.DirectorDTO;
+import com.app.Entity.Director;
+import com.app.Service.DirectorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class DirectorHandler {
+
+    private final DirectorService directorService;
+
+    @Autowired
+    public DirectorHandler(DirectorService directorService) {
+        this.directorService = directorService;
+    }
+
+    public Mono<ServerResponse> findAll(ServerRequest request){
+        Flux<Director> directors = directorService.findAll();
+        return ServerResponse.ok().body(directors, Director.class);
+    }
+
+    public Mono<ServerResponse> findById(ServerRequest request){
+        Long id = Long.valueOf(request.pathVariable("id"));
+        Mono<Director> directorMono = directorService.findById(id);
+        return directorMono.flatMap(director -> ServerResponse.ok().bodyValue(director)
+                .switchIfEmpty(ServerResponse.notFound().build()))
+                .onErrorResume(throwable -> ServerResponse.badRequest().bodyValue("Error occurred while find director: "+ throwable.getMessage()));
+    }
+
+    public Mono<ServerResponse> create(ServerRequest request){
+        Mono<DirectorDTO> directorMono = request.bodyToMono(DirectorDTO.class);
+        return directorMono.flatMap(directorDTO -> directorService.save(directorDTO)
+                .flatMap(savedDirector -> ServerResponse.ok().bodyValue(savedDirector))
+                .onErrorResume(throwable -> ServerResponse.badRequest().bodyValue("Error occurred while saving director: "+ throwable.getMessage()))
+        );
+    }
+
+    public Mono<ServerResponse> update(ServerRequest request){
+        Long id = Long.valueOf(request.pathVariable("id"));
+        Mono<DirectorDTO> directorMono = request.bodyToMono(DirectorDTO.class);
+        return directorMono.flatMap(directorDTO -> directorService.update(id,directorDTO))
+                .flatMap(savedDirector -> ServerResponse.ok().bodyValue(savedDirector))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> delete(ServerRequest request){
+        Long id = Long.valueOf(request.pathVariable("id"));
+        return directorService.delete(id)
+                .then(ServerResponse.ok().build())
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+}
