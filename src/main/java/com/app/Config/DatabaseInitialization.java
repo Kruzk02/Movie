ada47495715dfedf749976d15f196bd3a98d9b4a
Thien Phuc <thienphuc484@gmail.com>
2024-04-26 13:07:16 +0700

    Add DatabaseInitialization.java

---
+++ src/main/java/com/app/Config/DatabaseInitialization.java
@@ -0,0 +1,25 @@
+package com.app.Config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.r2dbc.core.DatabaseClient;
+
+@Configuration
+public class DatabaseInitialization {
+
+    @Autowired
+    private DatabaseClient databaseClient;
+
+    @PostConstruct
+    public void initializeDatabase(){
+        databaseClient.sql(
+                "CREATE TABLE IF NOT EXISTS Director(" +
+                "id SERIAL PRIMARY KEY," +
+                "first_name VARCHAR(255) NOT NULL," +
+                "last_name VARCHAR(255) NOT NULL," +
+                "nationality VARCHAR(255) NOT NULL," +
+                "birthDate DATE NOT NULL);"
+        ).fetch().rowsUpdated().block();
+    }
+}
