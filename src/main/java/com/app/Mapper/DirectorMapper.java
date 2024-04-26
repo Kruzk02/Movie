085c522dbe54a321089e8af7c9d756cad91e790c
Thien Phuc <thienphuc484@gmail.com>
2024-04-26 13:08:28 +0700

    Update Director DTO and Mapper.

--- src/main/java/com/app/Mapper/DirectorMapper.java
+++ src/main/java/com/app/Mapper/DirectorMapper.java
@@ -11,7 +11,6 @@ public interface DirectorMapper {
 
     DirectorMapper INSTANCE = Mappers.getMapper(DirectorMapper.class);
 
-    @Mapping(source = "id",target = "id")
     @Mapping(source = "firstName",target = "firstName")
     @Mapping(source = "lastName",target = "lastName")
     @Mapping(source = "birthDate",target = "birthDate")
