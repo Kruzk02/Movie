5d44153f4325100ce5b5b9595ca4135098c22d00
Thien Phuc <thienphuc484@gmail.com>
2024-04-26 13:08:28 +0700

    Update Director DTO and Mapper.

--- src/main/java/com/app/DTO/DirectorDTO.java
+++ src/main/java/com/app/DTO/DirectorDTO.java
@@ -6,19 +6,10 @@ import java.time.LocalDate;
 
 public class DirectorDTO {
 
-    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
     private String nationality;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
