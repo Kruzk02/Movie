951b6c58c1030cb163281d50d8bf6d8f25026564
Thien Phuc <thienphuc484@gmail.com>
2024-04-27 12:47:47 +0700

    Add ActorMapper.java

---
+++ src/main/java/com/app/Mapper/ActorMapper.java
@@ -0,0 +1,19 @@
+package com.app.Mapper;
+
+import com.app.DTO.ActorDTO;
+import com.app.Entity.Actor;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ActorMapper {
+
+    ActorMapper INSTANCE = Mappers.getMapper(ActorMapper.class);
+
+    @Mapping(source = "firstName",target = "firstName")
+    @Mapping(source = "lastName",target = "lastName")
+    @Mapping(source = "birthDate",target = "birthDate")
+    @Mapping(source = "nationality",target = "nationality")
+    Actor mapActorDtoToActor(ActorDTO actorDTO);
+}
