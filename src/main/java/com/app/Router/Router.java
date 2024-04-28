bc7bdec7e93af57ef9615ac73d87f8389601f9e0
Thien Phuc <thienphuc484@gmail.com>
2024-04-28 09:58:37 +0700

    Change name from DirectorRouter to Router.

--- src/main/java/com/app/Router/Router.java
+++ src/main/java/com/app/Router/Router.java
@@ -1,5 +1,6 @@
 package com.app.Router;
 
+import com.app.Handler.ActorHandler;
 import com.app.Handler.DirectorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,19 +15,31 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 public class Router {
 
     private final DirectorHandler directorHandler;
+    private final ActorHandler actorHandler;
 
     @Autowired
-    public Router(DirectorHandler directorHandler) {
+    public Router(DirectorHandler directorHandler, ActorHandler actorHandler) {
         this.directorHandler = directorHandler;
+        this.actorHandler = actorHandler;
     }
 
     @Bean
-    public RouterFunction<ServerResponse> routerFunction(){
+    public RouterFunction<ServerResponse> directorRouter(){
         return RouterFunctions
                 .route(GET("/directors"),directorHandler::findAll)
                 .andRoute(GET("/directors/{id}"),directorHandler::findById)
                 .andRoute(POST("/directors"),directorHandler::create)
                 .andRoute(PUT("/directors/{id}"),directorHandler::update)
                 .andRoute(DELETE("/directors/{id}"),directorHandler::delete);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> actorRouter(){
+        return RouterFunctions
+                .route(GET("/actors"),actorHandler::findAll)
+                .andRoute(GET("/actors/{id}"),actorHandler::findById)
+                .andRoute(POST("/actors"),actorHandler::save)
+                .andRoute(PUT("/actors/{id}"),actorHandler::update)
+                .andRoute(DELETE("/actors/{id}"),actorHandler::delete);
     }
 }
