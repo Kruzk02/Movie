55e6bb57b3e08c6be22b83b2d6115ee411e99f0a
Thien Phuc <thienphuc484@gmail.com>
2024-05-09 11:04:46 +0700

    Add MovieRatingDTO
    This file contains the MovieRatingDTO class, which serves as a Data Transfer Object for transferring movie and rating data between layers of the application.

---
+++ src/main/java/com/app/DTO/MovieRatingDTO.java
@@ -0,0 +1,23 @@
+package com.app.DTO;
+
+public class MovieRatingDTO {
+
+    private Long ratingId;
+    private Long movieId;
+
+    public Long getRatingId() {
+        return ratingId;
+    }
+
+    public void setRatingId(Long ratingId) {
+        this.ratingId = ratingId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+}
