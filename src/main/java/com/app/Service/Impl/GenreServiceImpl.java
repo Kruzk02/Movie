5f58e3b5b035ba48c7a49ab02309b4eca28cae69
Thien Phuc <thienphuc484@gmail.com>
2024-05-12 14:05:21 +0700

    Update GenreService

--- src/main/java/com/app/Service/Impl/GenreServiceImpl.java
+++ src/main/java/com/app/Service/Impl/GenreServiceImpl.java
@@ -1,11 +1,7 @@
 package com.app.Service.Impl;
 
-import com.app.DTO.GenreMovieDTO;
 import com.app.Entity.Genre;
-import com.app.Entity.GenreMoviePK;
 import com.app.Expection.GenreNotFound;
-import com.app.Mapper.GenreMovieMapper;
-import com.app.Repository.GenreMovieRepository;
 import com.app.Repository.GenreRepository;
 import com.app.Service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +13,10 @@ import reactor.core.publisher.Mono;
 public class GenreServiceImpl implements GenreService {
 
     private final GenreRepository genreRepository;
-    private final GenreMovieRepository genreMovieRepository;
 
     @Autowired
-    public GenreServiceImpl(GenreRepository genreRepository, GenreMovieRepository genreMovieRepository) {
+    public GenreServiceImpl(GenreRepository genreRepository) {
         this.genreRepository = genreRepository;
-        this.genreMovieRepository = genreMovieRepository;
     }
 
     @Override
@@ -35,12 +29,6 @@ public class GenreServiceImpl implements GenreService {
         return genreRepository.findById(id)
                 .switchIfEmpty(Mono.error(new GenreNotFound("Genre not found with a id: " + id)))
                 .log("Find a genre with a id: " + id);
-    }
-
-    @Override
-    public Mono<GenreMoviePK> save(GenreMovieDTO genreMovieDTO) {
-        GenreMoviePK genreMoviePK = GenreMovieMapper.INSTANCE.mappingDtoToEntity(genreMovieDTO);
-        return genreMovieRepository.save(genreMoviePK);
     }
 
 }
