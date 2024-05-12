df2a1e39813997678cf68eb3b447249945bb96ba
Thien Phuc <thienphuc484@gmail.com>
2024-05-12 14:07:11 +0700

    Update RatingService

--- src/main/java/com/app/Service/Impl/RatingServiceImpl.java
+++ src/main/java/com/app/Service/Impl/RatingServiceImpl.java
@@ -1,18 +1,11 @@
 package com.app.Service.Impl;
 
-import com.app.DTO.MovieRatingDTO;
-import com.app.DTO.RatingDTO;
-import com.app.Entity.MovieRatingPK;
 import com.app.Entity.Rating;
 import com.app.Expection.RatingNotFound;
-import com.app.Mapper.MovieRatingMapper;
-import com.app.Mapper.RatingMapper;
-import com.app.Repository.MovieRatingPkRepository;
 import com.app.Repository.RatingRepository;
 import com.app.Service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,12 +13,10 @@ import reactor.core.publisher.Mono;
 public class RatingServiceImpl implements RatingService {
 
     private final RatingRepository ratingRepository;
-    private final MovieRatingPkRepository movieRatingPkRepository;
 
     @Autowired
-    public RatingServiceImpl(RatingRepository ratingRepository,MovieRatingPkRepository movieRatingPkRepository) {
+    public RatingServiceImpl(RatingRepository ratingRepository) {
         this.ratingRepository = ratingRepository;
-        this.movieRatingPkRepository = movieRatingPkRepository;
     }
 
     @Override
@@ -37,22 +28,5 @@ public class RatingServiceImpl implements RatingService {
     public Mono<Rating> findById(Long id) {
         return ratingRepository.findById(id)
                 .switchIfEmpty(Mono.error(new RatingNotFound("Rating not found with a id: " + id)));
-    }
-
-    @Override
-    @Transactional
-    public Mono<MovieRatingPK> saveMovieRating (MovieRatingDTO MovieRatingDTO) {
-        MovieRatingPK movieRatingPK = MovieRatingMapper.INSTANCE.mappingDtoToEntity(MovieRatingDTO);
-        return movieRatingPkRepository.save(movieRatingPK);
-    }
-
-    @Override
-    public Mono<MovieRatingPK> update(RatingDTO ratingDTO) {
-        return null;
-    }
-
-    @Override
-    public Mono<Void> delete(Long id) {
-        return null;
     }
 }
--- src/main/java/com/app/Service/RatingService.java
+++ src/main/java/com/app/Service/RatingService.java
@@ -1,17 +1,10 @@
 package com.app.Service;
 
-import com.app.DTO.MovieRatingDTO;
-import com.app.DTO.RatingDTO;
-import com.app.Entity.MovieRatingPK;
 import com.app.Entity.Rating;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface RatingService {
-
     Flux<Rating> findAll();
     Mono<Rating> findById(Long id);
-    Mono<MovieRatingPK> saveMovieRating (MovieRatingDTO MovieRatingDTO);
-    Mono<MovieRatingPK> update(RatingDTO ratingDTO);
-    Mono<Void> delete(Long id);
 }
