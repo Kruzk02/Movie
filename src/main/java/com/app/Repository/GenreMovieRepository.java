89c0351f41390248c5ed35c509e6ae0e2e0e85bb
Thien Phuc <thienphuc484@gmail.com>
2024-05-12 13:35:41 +0700

    Update GenreMovieRepository

--- src/main/java/com/app/Repository/GenreMovieRepository.java
+++ src/main/java/com/app/Repository/GenreMovieRepository.java
@@ -1,7 +1,17 @@
 package com.app.Repository;
 
 import com.app.Entity.GenreMoviePK;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface GenreMovieRepository extends ReactiveCrudRepository<GenreMoviePK,Long> {
+    @Query("SELECT * FROM Genre_Movie WHERE movie_id = :movieId")
+    Flux<GenreMoviePK> findAllByMovieId(Long movieId);
+
+    @Modifying
+    @Query("DELETE FROM Genre_Movie WHERE movie_id = :movieId")
+    Mono<Void> deleteByMovieId(Long movieId);
 }
