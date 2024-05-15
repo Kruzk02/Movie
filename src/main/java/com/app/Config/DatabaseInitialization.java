c5cb5ee0231580b30a5daedd4fbd4015ebda8811
Thien Phuc <thienphuc484@gmail.com>
2024-05-15 10:56:08 +0700

    Update DatabaseInitialization

--- src/main/java/com/app/Config/DatabaseInitialization.java
+++ src/main/java/com/app/Config/DatabaseInitialization.java
@@ -14,91 +14,85 @@ public class DatabaseInitialization {
     @PostConstruct
     public void initializeDatabase(){
         String sql = """
-                CREATE TABLE IF NOT EXISTS Genre(
+                CREATE TABLE IF NOT EXISTS genre (
                     id SERIAL PRIMARY KEY,
                     name VARCHAR(255) NOT NULL
                 );
 
-                CREATE TABLE IF NOT EXISTS Rating(
+                CREATE TABLE IF NOT EXISTS rating (
                     id SERIAL PRIMARY KEY,
-                    rating DECIMAL(8, 2) NOT NULL
+                    rating DECIMAL(3, 1) NOT NULL CHECK (rating >= 0 AND rating <= 10)
                 );
 
-                CREATE TABLE IF NOT EXISTS Director(
+                CREATE TABLE IF NOT EXISTS director (
                     id SERIAL PRIMARY KEY,
                     first_name VARCHAR(255) NOT NULL,
                     last_name VARCHAR(255) NOT NULL,
                     nationality VARCHAR(255) NOT NULL,
-                    birthDate DATE NOT NULL
+                    birth_date DATE NOT NULL
                 );
 
-                CREATE TABLE IF NOT EXISTS Movie(
+                CREATE TABLE IF NOT EXISTS movie (
                     id SERIAL PRIMARY KEY,
-                    director_id INTEGER NOT NULL,
-                    genre_id INTEGER NOT NULL,
-                    rating_id INTEGER NOT NULL,
                     title VARCHAR(255) NOT NULL,
                     release_year DATE NOT NULL,
-                    length INTEGER NOT NULL,
-                    CONSTRAINT fk_movie_rating FOREIGN KEY (rating_id) REFERENCES Rating(id)
+                    length INTEGER NOT NULL
                 );
 
-                CREATE TABLE IF NOT EXISTS Actor(
+                CREATE TABLE IF NOT EXISTS actor (
                     id SERIAL PRIMARY KEY,
                     first_name VARCHAR(255) NOT NULL,
                     last_name VARCHAR(255) NOT NULL,
                     nationality VARCHAR(255) NOT NULL,
-                    birthDate DATE NOT NULL
+                    birth_date DATE NOT NULL
                 );
 
-                CREATE TABLE IF NOT EXISTS Genre_Movie(
-                    id SERIAL PRIMARY KEY,
+                CREATE TABLE IF NOT EXISTS genre_movie (
                     genre_id INTEGER NOT NULL,
                     movie_id INTEGER NOT NULL,
-                    CONSTRAINT fk_genre_movie_genre FOREIGN KEY (genre_id) REFERENCES Genre(id),
-                    CONSTRAINT fk_genre_movie_movie FOREIGN KEY (movie_id) REFERENCES Movie(id)
+                    PRIMARY KEY (genre_id, movie_id),
+                    FOREIGN KEY (genre_id) REFERENCES genre(id),
+                    FOREIGN KEY (movie_id) REFERENCES movie(id)
                 );
 
-                CREATE TABLE IF NOT EXISTS Director_Movie(
-                    id SERIAL PRIMARY KEY,
-                    director_id INTEGER NOT NULL,
-                    movie_id INTEGER NOT NULL,
-                    CONSTRAINT fk_director_movie_director FOREIGN KEY (director_id) REFERENCES Director(id),
-                    CONSTRAINT fk_director_movie_movie FOREIGN KEY (movie_id) REFERENCES Movie(id)
-                );
-
-                CREATE TABLE IF NOT EXISTS Actor_Movie (
-                    id SERIAL PRIMARY KEY,
+                CREATE TABLE IF NOT EXISTS actor_movie (
                     actor_id INTEGER NOT NULL,
                     movie_id INTEGER NOT NULL,
-                    CONSTRAINT fk_actor_movie_actor FOREIGN KEY (actor_id) REFERENCES Actor(id),
-                    CONSTRAINT fk_actor_movie_movie FOREIGN KEY (movie_id) REFERENCES Movie(id)
+                    PRIMARY KEY (actor_id, movie_id),
+                    FOREIGN KEY (actor_id) REFERENCES actor(id),
+                    FOREIGN KEY (movie_id) REFERENCES movie(id)
                 );
 
-                CREATE TABLE IF NOT EXISTS Movie_Rating(
-                    id SERIAL PRIMARY KEY,
+                CREATE TABLE IF NOT EXISTS director_movie (
+                    director_id INTEGER NOT NULL,
+                    movie_id INTEGER NOT NULL,
+                    PRIMARY KEY (director_id, movie_id),
+                    FOREIGN KEY (director_id) REFERENCES director(id),
+                    FOREIGN KEY (movie_id) REFERENCES movie(id)
+                );
+
+                CREATE TABLE IF NOT EXISTS movie_rating (
                     movie_id INTEGER NOT NULL,
                     rating_id INTEGER NOT NULL,
-                    CONSTRAINT fk_movie_rating_movie FOREIGN KEY (movie_id) REFERENCES Movie(id),
-                    CONSTRAINT fk_movie_rating_rating FOREIGN KEY (rating_id) REFERENCES Rating(id)
+                    PRIMARY KEY (movie_id, rating_id),
+                    FOREIGN KEY (movie_id) REFERENCES movie(id),
+                    FOREIGN KEY (rating_id) REFERENCES rating(id)
                 );
 
-                CREATE INDEX idx_movie_title ON Movie(title);
-                CREATE INDEX idx_movie_release_year ON Movie(release_year);
-                CREATE INDEX idx_movie_length ON Movie(length);
-                CREATE INDEX idx_genre_name ON Genre(name);
-                CREATE INDEX idx_director_first_name ON Director(first_name);
-                CREATE INDEX idx_director_last_name ON Director(last_name);
-                CREATE INDEX idx_actor_first_name ON Actor(first_name);
-                CREATE INDEX idx_actor_last_name ON Actor(last_name);
-                CREATE INDEX idx_director_movie_director_id ON Director_Movie(director_id);
-                CREATE INDEX idx_director_movie_movie_id ON Director_Movie(movie_id);
-                CREATE INDEX idx_actor_movie_actor_id ON Actor_Movie(actor_id);
-                CREATE INDEX idx_actor_movie_movie_id ON Actor_Movie(movie_id);
-                CREATE INDEX idx_genre_movie_genre_id ON Genre_Movie(genre_id);
-                CREATE INDEX idx_genre_movie_movie_id ON Genre_Movie(movie_id);
-                CREATE INDEX idx_movie_rating_movie_id ON Movie_Rating(movie_id);
-                CREATE INDEX idx_movie_rating_rating_id ON Movie_Rating(rating_id);
+                CREATE INDEX idx_movie_title ON movie(title);
+                CREATE INDEX idx_movie_release_year ON movie(release_year);
+                CREATE INDEX idx_movie_length ON movie(length);
+                CREATE INDEX idx_genre_name ON genre(name);
+                CREATE INDEX idx_director_first_name ON director(first_name);
+                CREATE INDEX idx_director_last_name ON director(last_name);
+                CREATE INDEX idx_actor_first_name ON actor(first_name);
+                CREATE INDEX idx_actor_last_name ON actor(last_name);
+                CREATE INDEX idx_genre_movie_genre_id ON genre_movie(genre_id);
+                CREATE INDEX idx_genre_movie_movie_id ON genre_movie(movie_id);
+                CREATE INDEX idx_actor_movie_actor_id ON actor_movie(actor_id);
+                CREATE INDEX idx_actor_movie_movie_id ON actor_movie(movie_id);
+                CREATE INDEX idx_movie_rating_movie_id ON movie_rating(movie_id);
+                CREATE INDEX idx_movie_rating_rating_id ON movie_rating(rating_id);
                 """;
         databaseClient.sql(sql).fetch().rowsUpdated().block();
     }
