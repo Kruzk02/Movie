df00dc0c3497ec98dfa495928ba6caf95775e2b8
Thien Phuc <thienphuc484@gmail.com>
2024-05-16 12:53:06 +0700

    Delete MovieRating DTO , Mapper and Service

--- src/main/java/com/app/Service/MovieRatingService.java
+++
@@ -1,9 +0,0 @@
-package com.app.Service;
-
-import com.app.DTO.MovieRatingDTO;
-import com.app.Entity.MovieRatingPK;
-import reactor.core.publisher.Mono;
-
-public interface MovieRatingService {
-    Mono<MovieRatingPK> save(MovieRatingDTO movieRatingDTO);
-}
