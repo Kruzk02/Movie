be28efd6cc26d29205cd50244419461bfa4383f1
Thien Phuc <thienphuc484@gmail.com>
2024-05-16 10:44:32 +0700

    Update GenreMovieRepository
    Delete deletByMovieId method and change @Query FindAllByMovieId method.

--- src/main/java/com/app/Repository/GenreMovieRepository.java
+++ src/main/java/com/app/Repository/GenreMovieRepository.java
@@ -1,17 +1,12 @@
 package com.app.Repository;
 
+import com.app.Entity.Genre;
 import com.app.Entity.GenreMoviePK;
-import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface GenreMovieRepository extends ReactiveCrudRepository<GenreMoviePK,Long> {
-    @Query("SELECT * FROM Genre_Movie WHERE movie_id = :movieId")
-    Flux<GenreMoviePK> findAllByMovieId(Long movieId);
-
-    @Modifying
-    @Query("DELETE FROM Genre_Movie WHERE movie_id = :movieId")
-    Mono<Void> deleteByMovieId(Long movieId);
+    @Query("SELECT * FROM genre_movie gm JOIN genre g ON gm.genre_id = g.id WHERE gm.movie_id = :movie_id")
+    Flux<Genre> findAllByMovieId(Long movieId);
 }
