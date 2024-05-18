750c133d4a08d92bd6701db8987c383a503afcf2
Thien Phuc <thienphuc484@gmail.com>
2024-05-18 09:11:56 +0700

    Update GenreMovieRepository
    Add a new method for find all movie by genre id.

--- src/main/java/com/app/Repository/GenreMovieRepository.java
+++ src/main/java/com/app/Repository/GenreMovieRepository.java
@@ -2,6 +2,7 @@ package com.app.Repository;
 
 import com.app.Entity.Genre;
 import com.app.Entity.GenreMoviePK;
+import com.app.Entity.Movie;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
@@ -9,4 +10,7 @@ import reactor.core.publisher.Flux;
 public interface GenreMovieRepository extends ReactiveCrudRepository<GenreMoviePK,Long> {
     @Query("SELECT * FROM genre_movie gm JOIN genre g ON gm.genre_id = g.id WHERE gm.movie_id = :movie_id")
     Flux<Genre> findAllByMovieId(Long movieId);
+
+    @Query("SELECT * FROM genre_movie gm JOIN movie m ON gm.movie_id = m.id WHERE gm.genre_id = :genre_id")
+    Flux<Movie> findAllMovieByGenreId(Long genreId);
 }
