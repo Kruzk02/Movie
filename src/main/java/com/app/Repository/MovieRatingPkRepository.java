7d14b0d15b5e05449e20f636ce98156e513ac1a4
Thien Phuc <thienphuc484@gmail.com>
2024-05-19 13:02:39 +0700

    Update MovieRatingPkRepository
    Change Query.

--- src/main/java/com/app/Repository/MovieRatingPkRepository.java
+++ src/main/java/com/app/Repository/MovieRatingPkRepository.java
@@ -1,18 +1,12 @@
 package com.app.Repository;
 
 import com.app.Entity.MovieRatingPK;
-import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface MovieRatingPkRepository extends ReactiveCrudRepository<MovieRatingPK,Long> {
 
-    @Query("SELECT * From movie_rating where movie_id = :movieId")
+    @Query("SELECT * From movie_rating mr JOIN rating r ON mr.rating_id = r.id where mr.movie_id = :movieId")
     Flux<MovieRatingPK> findAllByMovieId(Long movieId);
-
-    @Modifying
-    @Query("DELETE FROM movie_rating where movie_id = :movieId")
-    Mono<Void> deleteByMovieId(Long movieId);
 }
