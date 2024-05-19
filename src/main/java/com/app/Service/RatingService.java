ae36cdf9e1234130e90e07e7972eb22a1efd97ce
Thien Phuc <thienphuc484@gmail.com>
2024-05-19 13:04:52 +0700

    Update RatingService.
    Add save method and getAverageRating to RatingService.

--- src/main/java/com/app/Service/RatingService.java
+++ src/main/java/com/app/Service/RatingService.java
@@ -1,5 +1,7 @@
 package com.app.Service;
 
+import com.app.DTO.RatingDTO;
+import com.app.Entity.MovieRatingPK;
 import com.app.Entity.Rating;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -7,4 +9,6 @@ import reactor.core.publisher.Mono;
 public interface RatingService {
     Flux<Rating> findAll();
     Mono<Rating> findById(Long id);
+    Mono<MovieRatingPK> save(RatingDTO ratingDTO);
+    Mono<Double> getAverageRating(Long movieId);
 }
