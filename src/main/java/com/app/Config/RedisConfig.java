49f071e005f59fe63b36057720a6eee4f5265dc5
Thien Phuc <thienphuc484@gmail.com>
2024-05-20 13:10:19 +0700

    Update RedisConfig

--- src/main/java/com/app/Config/RedisConfig.java
+++ src/main/java/com/app/Config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.app.Config;
 
+import com.app.Entity.Actor;
+import com.app.Entity.Director;
 import com.app.Entity.Movie;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -7,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -22,14 +23,40 @@ public class RedisConfig {
     }
 
     @Bean
-    public ReactiveRedisTemplate<String, Movie> reactiveRedisTemplate(
-            ReactiveRedisConnectionFactory factory) {
+    public ReactiveRedisTemplate<String, Movie> movieReactiveRedisTemplate(
+            ReactiveRedisConnectionFactory factory,ObjectMapper objectMapper) {
         StringRedisSerializer keySerializer = new StringRedisSerializer();
         Jackson2JsonRedisSerializer<Movie> valueSerializer =
-                new Jackson2JsonRedisSerializer<>(objectMapper(),Movie.class);
+                new Jackson2JsonRedisSerializer<>(objectMapper,Movie.class);
         RedisSerializationContext.RedisSerializationContextBuilder<String, Movie> builder =
                 RedisSerializationContext.newSerializationContext(keySerializer);
         RedisSerializationContext<String, Movie> context =
+                builder.value(valueSerializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, Actor> actorReactiveRedisTemplate(
+            ReactiveRedisConnectionFactory factory,ObjectMapper objectMapper) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Actor> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper,Actor.class);
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Actor> builder =
+                RedisSerializationContext.newSerializationContext(keySerializer);
+        RedisSerializationContext<String, Actor> context =
+                builder.value(valueSerializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, Director> directorReactiveRedisTemplate(
+            ReactiveRedisConnectionFactory factory,ObjectMapper objectMapper) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Director> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper,Director.class);
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Director> builder =
+                RedisSerializationContext.newSerializationContext(keySerializer);
+        RedisSerializationContext<String, Director> context =
                 builder.value(valueSerializer).build();
         return new ReactiveRedisTemplate<>(factory, context);
     }
