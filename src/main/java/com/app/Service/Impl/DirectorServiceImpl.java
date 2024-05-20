cfcbb4b68d104f9c59dd6f36f9e1723900b2b39b
Thien Phuc <thienphuc484@gmail.com>
2024-05-20 13:08:52 +0700

    Update DirectorServiceImpl
    Add Caching to findAll and findByid method.

--- src/main/java/com/app/Service/Impl/DirectorServiceImpl.java
+++ src/main/java/com/app/Service/Impl/DirectorServiceImpl.java
@@ -3,34 +3,52 @@ package com.app.Service.Impl;
 import com.app.DTO.DirectorDTO;
 import com.app.Entity.Director;
 import com.app.Entity.Nationality;
+import com.app.Expection.ActorNotFound;
 import com.app.Expection.DirectorNotFound;
 import com.app.Mapper.DirectorMapper;
 import com.app.Repository.DirectorRepository;
 import com.app.Service.DirectorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 public class DirectorServiceImpl implements DirectorService {
 
     private final DirectorRepository directorRepository;
+    private final ReactiveRedisTemplate<String,Director> redisTemplate;
 
     @Autowired
-    public DirectorServiceImpl(DirectorRepository directorRepository) {
+    public DirectorServiceImpl(DirectorRepository directorRepository, ReactiveRedisTemplate<String, Director> redisTemplate) {
         this.directorRepository = directorRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
     public Flux<Director> findAll() {
-        return directorRepository.findAll();
+        return redisTemplate.keys("director:*")
+                .flatMap(key -> redisTemplate.opsForValue().get(key))
+                .thenMany(directorRepository.findAll()
+                        .flatMap(director -> redisTemplate
+                                .opsForValue()
+                                .set("director:" + director.getId(),director, Duration.ofHours(24))
+                                .thenReturn(director)))
+                .log("Find all directors");
     }
 
     @Override
     public Mono<Director> findById(Long id) {
-        return directorRepository.findById(id)
-                .switchIfEmpty(Mono.error(new DirectorNotFound("Director Not Found with a id: " + id)))
+        return redisTemplate.opsForValue().get("director:"+ id)
+                .switchIfEmpty(directorRepository.findById(id)
+                        .switchIfEmpty(Mono.error(new ActorNotFound("Actor not found with a id: " + id)))
+                        .flatMap(director -> redisTemplate
+                                .opsForValue()
+                                .set("actor:" + director.getId(),director, Duration.ofHours(24))
+                                .thenReturn(director)))
                 .log("Find a Director with a id: " + id);
     }
 
