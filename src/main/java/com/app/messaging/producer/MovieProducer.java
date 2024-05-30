9e89c8cf5518b2de9741c8c03170aa43ca7023f4
Thien Phuc <thienphuc484@gmail.com>
2024-05-30 13:28:58 +0700

    Add MovieProducer.java
    Create MovieProducer class to facilitate data communition between Movie Service class and other related class.

---
+++ src/main/java/com/app/messaging/producer/MovieProducer.java
@@ -0,0 +1,31 @@
+package com.app.messaging.producer;
+
+import com.app.DTO.MovieDTO;
+import com.app.Entity.Movie;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieProducer {
+
+    private static final Logger log = LogManager.getLogger(MovieProducer.class);
+    private final AmqpTemplate amqpTemplate;
+
+    @Autowired
+    public MovieProducer(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
+
+    public void sendMovie(Movie movie){
+        try{
+            amqpTemplate.convertAndSend("genre-exchange","movie-routing-key",movie);
+            log.info("Successfully send messaging [{}] to movie-queue",movie);
+        }catch (AmqpException e){
+            log.error("Failed to send messaging [{}] to movie-queue",movie,e);
+        }
+    }
+}
