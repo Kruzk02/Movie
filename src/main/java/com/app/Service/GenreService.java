18a94ed654897790fcd73ff84564a63ec44beb82
Thien Phuc <thienphuc484@gmail.com>
2024-05-31 10:36:17 +0700

    Update GenreService class

--- src/main/java/com/app/Service/GenreService.java
+++ src/main/java/com/app/Service/GenreService.java
@@ -1,10 +1,14 @@
 package com.app.Service;
 
 import com.app.Entity.Genre;
+import com.app.Entity.GenreMoviePK;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Set;
 
 public interface GenreService {
     Flux<Genre> findAll();
     Mono<Genre> findById(Long id);
+    Mono<GenreMoviePK> save(Long genreId,Long movieId);
 }
