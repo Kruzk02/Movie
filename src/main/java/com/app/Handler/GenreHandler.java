88647bdd201a77f693b6565c5209f8b030a2b01a
Thien Phuc <thienphuc484@gmail.com>
2024-05-31 10:37:34 +0700

    Update GenreHandler class

--- src/main/java/com/app/Handler/GenreHandler.java
+++ src/main/java/com/app/Handler/GenreHandler.java
@@ -1,8 +1,11 @@
 package com.app.Handler;
 
+import com.app.DTO.GenreDTO;
 import com.app.Entity.Genre;
 import com.app.Service.GenreService;
+import com.app.messaging.processor.MovieProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -12,10 +15,12 @@ import reactor.core.publisher.Mono;
 public class GenreHandler {
 
     private final GenreService genreService;
+    private final MovieProcessor movieProcessor;
 
     @Autowired
-    public GenreHandler(GenreService genreService) {
+    public GenreHandler(GenreService genreService, MovieProcessor movieProcessor) {
         this.genreService = genreService;
+        this.movieProcessor = movieProcessor;
     }
 
     public Mono<ServerResponse> findAll(ServerRequest request){
@@ -27,5 +32,14 @@ public class GenreHandler {
         Mono<Genre> genreMono = genreService.findById(id);
         return genreMono.flatMap(genre -> ServerResponse.ok().bodyValue(genre))
                 .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> save(ServerRequest request){
+        return request.bodyToMono(GenreDTO.class)
+                .flatMap(genreDTO -> movieProcessor.getMovie()
+                        .flatMap(movie -> genreService.save(genreDTO.getGenreId(),movie.getId()))
+                        .flatMap(savedGenreMovie -> ServerResponse.ok().bodyValue(savedGenreMovie))
+                        .switchIfEmpty(ServerResponse.notFound().build())
+                        .onErrorResume(error -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Error saving genre: " + error.getMessage())));
     }
 }
