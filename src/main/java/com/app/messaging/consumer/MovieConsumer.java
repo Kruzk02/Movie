f3dc41e044a28c42ea6323cb9b721ab4564a41ed
Thien Phuc <thienphuc484@gmail.com>
2024-06-02 12:05:16 +0700

    Update MovieCOnsumer

--- src/main/java/com/app/messaging/consumer/MovieConsumer.java
+++ src/main/java/com/app/messaging/consumer/MovieConsumer.java
@@ -1,31 +1,36 @@
 package com.app.messaging.consumer;
 
 import com.app.Entity.Movie;
-import com.app.messaging.processor.MovieProcessor;
+import com.app.messaging.processor.Processor;
+import org.springframework.stereotype.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class MovieConsumer {
 
     private static final Logger log = LogManager.getLogger(MovieConsumer.class);
-    private final MovieProcessor movieProcessor;
+    private final List<Processor<Movie>> processors;
 
     @Autowired
-    public MovieConsumer(MovieProcessor movieProcessor) {
-        this.movieProcessor = movieProcessor;
+    public MovieConsumer(List<Processor<Movie>> processors) {
+        this.processors = processors;
     }
 
     @RabbitListener(queues = "movie-queue")
     public void receiveMovie(Movie movie){
         try{
             log.info("Successfully receive [{}] from movie-queue",movie);
-            movieProcessor.processMovie(movie);
+            for (Processor<Movie> processor : processors) {
+                processor.processMovie(movie);
+            }
         }catch (Exception e){
             log.error("Failed receive [{}] from movie-queue",movie,e);
         }
     }
 }
+
