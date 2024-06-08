faf8e7e7f4d185f676600f621257dd4da6b23f69
Thien Phuc <thienphuc484@gmail.com>
2024-06-08 11:36:12 +0700

    Update Repository

--- src/main/java/com/app/Repository/GenreMovieRepository.java
+++ src/main/java/com/app/Repository/GenreMovieRepository.java
@@ -6,11 +6,12 @@ import com.app.Entity.Movie;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface GenreMovieRepository extends ReactiveCrudRepository<GenreMoviePK,Long> {
     @Query("SELECT * FROM genre_movie gm JOIN genre g ON gm.genre_id = g.id WHERE gm.movie_id = :movie_id")
     Flux<Genre> findAllByMovieId(Long movieId);
 
-    @Query("SELECT * FROM genre_movie gm JOIN movie m ON gm.movie_id = m.id WHERE gm.genre_id = :genre_id")
-    Flux<Movie> findAllMovieByGenreId(Long genreId);
+    @Query("SELECT g.* FROM genre g JOIN genre_movie gm ON g.id = gm.genre_id JOIN movie m ON gm.movie_id = m.id WHERE m.id = :movieId")
+    Flux<Genre> findByMovieId(Long movieId);
 }
