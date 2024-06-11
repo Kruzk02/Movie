da4cd6fdd9df4bfbaf0d5292c9a9cfc3ee25afbd
Thien Phuc <thienphuc484@gmail.com>
2024-06-11 12:08:29 +0700

    Update Repository
    Add a method for find a movie.

--- src/main/java/com/app/Repository/DirectorMovieRepository.java
+++ src/main/java/com/app/Repository/DirectorMovieRepository.java
@@ -2,6 +2,7 @@ package com.app.Repository;
 
 import com.app.Entity.Director;
 import com.app.Entity.DirectorMoviePK;
+import com.app.Entity.Movie;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
@@ -9,4 +10,7 @@ import reactor.core.publisher.Flux;
 public interface DirectorMovieRepository extends ReactiveCrudRepository<DirectorMoviePK,Long> {
     @Query("SELECT d.* FROM director d JOIN director_movie dm ON d.id = dm.director_id JOIN movie m ON dm.movie_id = m.id WHERE m.id = :movieId")
     Flux<Director> findDirectorByMovieId(Long movieId);
+
+    @Query("SELECT m.* FROM movie m JOIN director_movie dm ON m.id = dm.movie_id JOIN director d ON dm.director_id = d.id WHERE d.id = :id")
+    Flux<Movie> findMovieByDirectorId(Long id);
 }
