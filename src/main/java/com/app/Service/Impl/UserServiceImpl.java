e58fced884327e595ba772f1cb2bbafaebec980b
Thien Phuc <thienphuc484@gmail.com>
2024-06-24 11:36:19 +0700

    Update: UserService
    Add validation for email,password and phone number

--- src/main/java/com/app/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/app/Service/Impl/UserServiceImpl.java
@@ -3,12 +3,14 @@ package com.app.Service.Impl;
 import com.app.DTO.UserDTO;
 import com.app.Entity.User;
 import com.app.Entity.UserRolePK;
-import com.app.Expection.UserNotFound;
+import com.app.Expection.*;
 import com.app.Mapper.UserMapper;
 import com.app.Repository.RoleRepository;
 import com.app.Repository.UserRepository;
 import com.app.Repository.UserRoleRepository;
 import com.app.Service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,9 +19,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final Logger log = LogManager.getLogger(UserServiceImpl.class);
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final UserRoleRepository userRoleRepository;
@@ -81,6 +87,19 @@ public class UserServiceImpl implements UserService {
     public Mono<User> register(UserDTO userDTO) {
         User user = UserMapper.INSTANCE.mapDTOToEntity(userDTO);
         user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        if (!validateEmail(user.getEmail())){
+            throw new EmailValidationException("Invalid email address format: " + user.getEmail());
+        }
+
+        if (!validatePassword(user.getPassword())){
+            throw new PasswordValidationException();
+        }
+
+        if (!validatePhoneNumber(user.getPhoneNumber())){
+            throw new PhoneNumberValidationException("Invalid phone number format: " + user.getPhoneNumber());
+        }
+
         return userRepository.save(user)
                 .flatMap(savedUser -> roleRepository.findByName("ROLE_USER")
                         .flatMap(roleUser -> userRoleRepository.save(new UserRolePK(savedUser.getId(),roleUser.getId())))
@@ -94,5 +113,27 @@ public class UserServiceImpl implements UserService {
             .switchIfEmpty(Mono.error(new UserNotFound("User not found with a id: " + id)))
             .flatMap(userRepository::delete)
             .log("Delete user with a id: " + id);
+    }
+
+    //thienphuc123456@gmail.com
+    private boolean validateEmail(String email){
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    //SecureP@ssw0rd
+    private boolean validatePassword(String password) {
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    //+84 35 539-0605
+    private boolean validatePhoneNumber(String phoneNumber){
+        Pattern pattern = Pattern.compile("^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
     }
 }
