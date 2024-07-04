7efd077716e7e3cdf16657cddd0a8270d3bda92f
Thien Phuc <thienphuc484@gmail.com>
2024-07-04 11:57:50 +0700

    Add EmailService class and interface

---
+++ src/main/java/com/app/Service/Impl/EmailServiceImpl.java
@@ -0,0 +1,49 @@
+package com.app.Service.Impl;
+
+import com.app.Service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public Mono<Void> sendVerificationEmail(String to, String token) {
+        return Mono.fromRunnable(() -> {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper;
+            try {
+                helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                helper.setTo(to);
+                helper.setSubject("Verify Your Account");
+
+                String htmlContent = "<html>" +
+                        "<body>" +
+                        "<h1>Verify Your Account</h1>" +
+                        "<p>To verify your account, click on the following link:</p>" +
+                        "<a href=\"http://localhost:8080/users/verify?token=" + token + "\">Verify Now</a>" +
+                        "</body>" +
+                        "</html>";
+
+                helper.setText(htmlContent, true);
+            } catch (MessagingException e) {
+                throw new RuntimeException("Failed to send email", e);
+            }
+            javaMailSender.send(mimeMessage);
+        }).then();
+    }
+}
