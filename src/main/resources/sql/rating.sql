1c9253bb79c1ce7dc01365d4ab257ac3e94c4485
thienphuc <thienphuc484@gmail.com>
2024-07-05 10:36:12 +0700

    Update: SQL

--- src/main/resources/sql/rating.sql
+++ src/main/resources/sql/rating.sql
@@ -1,4 +1,8 @@
 CREATE TABLE IF NOT EXISTS "rating" (
     id SERIAL PRIMARY KEY,
-    rating DECIMAL(3, 1) NOT NULL CHECK (rating >= 0 AND rating <= 10)
+    user_id int,
+    movie_id int,
+    rating DECIMAL(3, 1) NOT NULL CHECK (rating >= 0 AND rating <= 5),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
 );
