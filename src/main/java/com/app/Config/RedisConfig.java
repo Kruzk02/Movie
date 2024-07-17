0d1c62e2af85f17abf0482be0624842927207bc6
thienphuc <thienphuc484@gmail.com>
2024-07-17 11:36:44 +0700

    Update RedisConfig class

--- src/main/java/com/app/Config/RedisConfig.java
+++ src/main/java/com/app/Config/RedisConfig.java
@@ -2,9 +2,21 @@ package com.app.Config;
 
 import com.app.Entity.Actor;
 import com.app.Entity.Director;
+import com.app.Entity.Genre;
 import com.app.Entity.Movie;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,12 +26,35 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.*;
 
+import java.time.Duration;
+import java.util.function.Supplier;
+
 @Configuration
 public class RedisConfig {
 
     @Primary
     public ReactiveRedisConnectionFactory lettuceConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+    }
+
+    @Bean
+    public ProxyManager<String> lettuceBasedProxyManager(){
+        RedisClient redisClient = RedisClient.create(RedisURI.builder()
+                .withHost("localhost")
+                .withPort(6379)
+                .withSsl(false)
+                .build());
+        StatefulRedisConnection<String,byte[]> redisConnection = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+
+        return LettuceBasedProxyManager.builderFor(redisConnection)
+                .withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(1L))).build();
+    }
+
+    @Bean
+    public Supplier<BucketConfiguration> bucketConfiguration(){
+        return () -> BucketConfiguration.builder()
+                .addLimit(Bandwidth.builder().capacity(200L).refillGreedy(200L,Duration.ofMinutes(1L)).build())
+                .build();
     }
 
     @Bean
@@ -57,6 +92,19 @@ public class RedisConfig {
         RedisSerializationContext.RedisSerializationContextBuilder<String, Director> builder =
                 RedisSerializationContext.newSerializationContext(keySerializer);
         RedisSerializationContext<String, Director> context =
+                builder.value(valueSerializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, Genre> genreReactiveRedisTemplate(
+            ReactiveRedisConnectionFactory factory,ObjectMapper objectMapper) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Genre> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper,Genre.class);
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Genre> builder =
+                RedisSerializationContext.newSerializationContext(keySerializer);
+        RedisSerializationContext<String, Genre> context =
                 builder.value(valueSerializer).build();
         return new ReactiveRedisTemplate<>(factory, context);
     }
