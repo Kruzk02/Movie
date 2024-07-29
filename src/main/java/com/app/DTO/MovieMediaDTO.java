8862f2f4333a7af64889a40e5d87aafb64d5d9c6
thienphuc <thienphuc484@gmail.com>
2024-07-29 12:42:26 +0700

    Add Movie Media DTO and Mapper

---
+++ src/main/java/com/app/DTO/MovieMediaDTO.java
@@ -0,0 +1,43 @@
+package com.app.DTO;
+
+import java.time.LocalTime;
+
+public class MovieMediaDTO {
+
+    private Long movieId;
+    private byte episodes;
+    private LocalTime duration;
+    private String quality;
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public byte getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(byte episodes) {
+        this.episodes = episodes;
+    }
+
+    public LocalTime  getDuration() {
+        return duration;
+    }
+
+    public void setDuration(LocalTime duration) {
+        this.duration = duration;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+}
---
+++ src/main/java/com/app/Mapper/MovieMediaMapper.java
@@ -0,0 +1,18 @@
+package com.app.Mapper;
+
+import com.app.DTO.MovieMediaDTO;
+import com.app.Entity.MovieMedia;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MovieMediaMapper {
+    MovieMediaMapper INSTANCE = Mappers.getMapper(MovieMediaMapper.class);
+
+    @Mapping(source = "movieId",target = "movieId")
+    @Mapping(source = "episodes",target = "episodes")
+    @Mapping(source = "duration",target = "duration")
+    @Mapping(source = "quality",target = "quality")
+    MovieMedia mapDtoToEntity(MovieMediaDTO movieMediaDTO);
+}
