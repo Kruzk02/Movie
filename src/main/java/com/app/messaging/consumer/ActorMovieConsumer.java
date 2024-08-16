76634a5b7a7d167e3fc15322b986fc224d561837
thienphuc <thienphuc484@gmail.com>
2024-08-16 09:13:40 +0700

    Add ActorMovieConsumer

---
+++ src/main/java/com/app/messaging/consumer/ActorMovieConsumer.java
@@ -0,0 +1,35 @@
+package com.app.messaging.consumer;
+
+import com.app.Entity.Movie;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Component
+public class ActorMovieConsumer {
+
+    private static final Logger log = LogManager.getLogger(ActorMovieConsumer.class);
+    private final Queue<Movie> queue = new ConcurrentLinkedQueue<>();
+    @Autowired private ObjectMapper objectMapper;
+
+    public Movie receive() {
+        return queue.poll();
+    }
+
+    @KafkaListener(topics = "movie-data", groupId = "actor-group",containerFactory = "actorKafkaListenerContainerFactory")
+    private void listen(byte[] data) throws IOException {
+        try {
+            Movie movie = objectMapper.readValue(data,Movie.class);
+            queue.offer(movie);
+        } catch (IOException e) {
+            log.error("Error deserializing Movie object: {}", e.getMessage());
+        }
+    }
+}
