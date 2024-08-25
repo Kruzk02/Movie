3045a5974169615f1414b8d2fe1f28c5ba08f56e
thienphuc <thienphuc484@gmail.com>
2024-08-25 09:20:02 +0700

    Add AppConfig and constants

---
+++ src/main/java/com/app/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.app.constants;
+
+public class AppConstants {
+
+    public static final String MOVIE_POSTER = "moviePoster/";
+    public static final String DIRECTOR_PHOTO = "directorPhoto/";
+    public static final String ACTOR_PHOTO = "actorPhoto/";
+    public static final String MOVIE_MEDIA = "movieMedia/";
+}
