aec255f0c70717e5abc3a595a864d38173bdefcb
thienphuc <thienphuc484@gmail.com>
2024-08-27 12:55:49 +0700

    Add CorsConfig

---
+++ src/main/java/com/app/Config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.app.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/")
+                .allowedMethods("*")
+                .maxAge(36000);
+    }
+}
