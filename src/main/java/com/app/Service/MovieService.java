60aa5384b1d32697a1731c06d80002631bfb8415
thienphuc <thienphuc484@gmail.com>
2024-08-31 11:44:33 +0700

    Update MovieService

--- src/main/java/com/app/Service/MovieService.java
+++ src/main/java/com/app/Service/MovieService.java
@@ -2,13 +2,13 @@ package com.app.Service;
 
 import com.app.DTO.MovieDTO;
 import com.app.Entity.Movie;
-import org.springframework.http.codec.multipart.FilePart;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface MovieService {
     Flux<Movie> findAll();
     Mono<Movie> findById(Long id);
+    Mono<Movie> findByIdAndReceiveUserId(Long id,Long userId);
     Mono<Movie> save(MovieDTO movieDTO);
     Mono<Movie> update(Long id,MovieDTO movieDTO);
     Mono<Void> delete(Long id);
