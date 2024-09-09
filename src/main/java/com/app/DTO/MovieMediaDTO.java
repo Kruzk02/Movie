ba113f83aa517c65feab51a9d6dfc132acd3f31b
thienphuc <thienphuc484@gmail.com>
2024-09-09 10:46:32 +0700

    Update Movie and MovieDTO
    Add Lombok annnotations

--- src/main/java/com/app/DTO/MovieMediaDTO.java
+++ src/main/java/com/app/DTO/MovieMediaDTO.java
@@ -1,52 +1,20 @@
 package com.app.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MovieMediaDTO {
-
     private Long movieId;
     private byte episodes;
     private LocalTime duration;
     private String quality;
     private String video;
-
-    public Long getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(Long movieId) {
-        this.movieId = movieId;
-    }
-
-    public byte getEpisodes() {
-        return episodes;
-    }
-
-    public void setEpisodes(byte episodes) {
-        this.episodes = episodes;
-    }
-
-    public LocalTime  getDuration() {
-        return duration;
-    }
-
-    public void setDuration(LocalTime duration) {
-        this.duration = duration;
-    }
-
-    public String getQuality() {
-        return quality;
-    }
-
-    public void setQuality(String quality) {
-        this.quality = quality;
-    }
-
-    public String getVideo() {
-        return video;
-    }
-
-    public void setVideo(String video) {
-        this.video = video;
-    }
 }
