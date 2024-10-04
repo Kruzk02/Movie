b1c9f5992b333e1de494669b412b80effe8e5c09
thienphuc <thienphuc484@gmail.com>
2024-10-04 12:56:54 +0700

    Add MovieEventDeserializer to deserializing movie event objects.

---
+++ src/main/java/com/app/serde/Deserializer/MovieEventDeserializer.java
@@ -0,0 +1,24 @@
+package com.app.serde.Deserializer;
+
+import com.app.Entity.MovieEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class MovieEventDeserializer implements Deserializer<MovieEvent> {
+
+    private final ObjectMapper objectMapper;
+
+    public MovieEventDeserializer() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+    @Override
+    public MovieEvent deserialize(String topic, byte[] data) {
+        try {
+            return objectMapper.readValue(data, MovieEvent.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Deserialization error", e);
+        }
+    }
+}
