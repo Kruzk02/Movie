616d801fc3f77a471d4f9c16b2fecd4586a3e2c3
thienphuc <thienphuc484@gmail.com>
2024-10-06 11:24:06 +0700

    Update rating service

--- src/main/java/com/app/Service/Impl/RatingServiceImpl.java
+++ src/main/java/com/app/Service/Impl/RatingServiceImpl.java
@@ -2,46 +2,60 @@ package com.app.Service.Impl;
 
 import com.app.DTO.RatingDTO;
 import com.app.Entity.Rating;
-import com.app.Expection.MovieNotMatching;
-import com.app.Expection.UserNotFound;
-import com.app.Expection.UserNotMatching;
-import com.app.Expection.RatingNotFound;
+import com.app.Expection.*;
 import com.app.Mapper.RatingMapper;
 import com.app.Repository.RatingRepository;
 import com.app.Service.RatingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Objects;
 
 @Service
+@AllArgsConstructor
 public class RatingServiceImpl implements RatingService {
 
     private final RatingRepository ratingRepository;
+    private final ReactiveRedisTemplate<String,Rating> ratingRedisTemplate;
+    private final ReactiveRedisTemplate<String,Double> doubleRedisTemplate;
 
-    @Autowired
-    public RatingServiceImpl(RatingRepository ratingRepository) {
-        this.ratingRepository = ratingRepository;
+    @Override
+    public Mono<Double> findAvgRatingByMovieId(Long movieId) {
+        return doubleRedisTemplate.opsForValue().get("avgRatting::movieId::" + movieId)
+            .switchIfEmpty(ratingRepository.findAvgRatingByMovieId(movieId)
+                .flatMap(avgRating -> doubleRedisTemplate.opsForValue()
+                    .set("avgRatting::movieId::" + movieId,avgRating,Duration.ofHours(2))
+                    .thenReturn(avgRating)
+                )
+            );
     }
 
     @Override
-    public Mono<Rating> findById(Long id) {
-        return ratingRepository.findById(id)
-                .switchIfEmpty(Mono.error(new RatingNotFound("Rating not found with a id: " + id)));
-    }
-
-    @Override
-    public Flux<Rating> findAllByMovieId(Long movieId) {
-        return ratingRepository.findAllByMovieId(movieId);
+    public Mono<Rating> findRatingByMovieIdAndUserId(Long movieId, Long userId) {
+         return ratingRedisTemplate.opsForValue().get("rating::movieId::" + movieId + "::userId::" + userId)
+             .switchIfEmpty(ratingRepository.findRatingByMovieIdAndUserId(movieId,userId)
+                 .flatMap(rating -> ratingRedisTemplate.opsForValue()
+                     .set("rating::movieId::" + movieId + "::userId::" + userId,rating,Duration.ofHours(2))
+                     .thenReturn(rating)
+                 )
+             );
     }
 
     @Override
     public Mono<Rating> save(RatingDTO ratingDTO,Long userId) {
         Rating rating = RatingMapper.INSTANCE.mappingDtoToEntity(ratingDTO);
         rating.setUserId(userId);
-        return ratingRepository.save(rating);
+
+        return ratingRepository.findRatingByMovieIdAndUserId(rating.getMovieId(),rating.getUserId())
+            .switchIfEmpty(ratingRepository.save(rating)
+                .flatMap(savedRating -> ratingRedisTemplate.opsForValue()
+                    .set("rating::movieId::" + savedRating.getMovieId() + "::userId::" + savedRating.getUserId(),savedRating,Duration.ofHours(2))
+                    .thenReturn(savedRating)
+                )
+            ).onErrorResume(e -> Mono.error(new RatingSameMovieException("Hey, you can't do that, mate.")));
     }
 
     @Override
@@ -58,7 +72,10 @@ public class RatingServiceImpl implements RatingService {
                 existingRating.setUserId(userId);
                 existingRating.setMovieId(ratingDTO.getMovieId());
                 return ratingRepository.save(existingRating);
-            });
+            })
+            .flatMap(updatedRating -> ratingRedisTemplate.opsForValue()
+                    .set("rating::movieId::" + updatedRating.getMovieId() + "::userId::" + updatedRating.getUserId(),updatedRating,Duration.ofHours(2))
+                    .thenReturn(updatedRating));
     }
 
     @Override
