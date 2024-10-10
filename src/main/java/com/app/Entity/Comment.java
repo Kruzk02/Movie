5a271b9c9f3673179f3acde13c4444424edf7fdd
thienphuc <thienphuc484@gmail.com>
2024-10-10 10:32:47 +0700

    Update JwtFilter'

--- src/main/java/com/app/Entity/Comment.java
+++ src/main/java/com/app/Entity/Comment.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 @Table("comments")
 public class Comment implements Serializable {
+    @Id
     private Long id;
     private Long userId;
     private Long movieId;
